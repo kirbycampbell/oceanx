125afa04dfb7fbeef7e637478a84c566a46fd261
kirbycampbell <jkirbycampbell@gmail.com>
2018-12-02 16:36:03 -0800

    FIxed KEy visibility

--- client/src/Redux/actions/barActions.jsx
+++ client/src/Redux/actions/barActions.jsx
@@ -3,7 +3,7 @@ import * as types from "../constants/actionTypes";
 export const googleSearch = query => {
   return dispatch => {
     return fetch(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}+Bar&key=AIzaSyDIPsk4vwU5ZXQPJTehPSxgKkX6sdfXQIw`
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}+Bar&key=MYKEY`
     )
       .then(response => response.json())
       .then(bar => {
--- client/src/Redux/constants/googleMapClient.js
+++
@@ -1,3 +0,0 @@
-export const googleMapsClient = require("@google/maps").createClient({
-  key: "AIzaSyDIPsk4vwU5ZXQPJTehPSxgKkX6sdfXQIw"
-});
